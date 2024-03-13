48e3ac19d59055e3039e2f0d5c8be73a339f1f87
Sarvesh-Sathish <sarveshsathish.2004@gmail.com>
2024-03-13 17:56:46 -0400

    added basic whodidthismodal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { Text, List, ListItem, ListIcon, Box } from '@chakra-ui/react';
 import {
-    Box,
     Center,
     VStack,
     Heading,
@@ -19,9 +19,16 @@ import {
 import { TabCard } from './TabCard';
 import { NavTitleLogoCard } from './NavTitleLogoCard';
 import { LogCard } from './LogCard';
+import { MdCheckCircle } from 'react-icons/md';
 
 export const Footer = () => {
+    const [isWhoDidThisModalOpen, setWhoDidThisModalOpen] = useState(false);
+
+    const openWhoDidThisModal = () => setWhoDidThisModalOpen(true);
+    const closeWhoDidThisModal = () => setWhoDidThisModalOpen(false);
+
     return (
+    <>
         <HStack
         bg="white" 
         height='9vh'
@@ -31,7 +38,7 @@ export const Footer = () => {
         px='20px'>
             <p>Cognicare © 2024</p>
             <HStack>
-                <Button colorScheme="blue" size="md" variant="outline">
+                <Button colorScheme="blue" size="md" variant="outline" onClick={openWhoDidThisModal}>
                     Who did this?
                 </Button>
                 <Button colorScheme="blue" size="md" variant="outline">
@@ -41,6 +48,54 @@ export const Footer = () => {
                     EULA
                 </Button>
             </HStack>
-    </HStack>
+        </HStack>
+        <Modal isOpen={isWhoDidThisModalOpen} onClose={closeWhoDidThisModal}>
+            <ModalOverlay />
+            <ModalContent>
+            <ModalHeader>Who did this?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            <Box mb={4}>
+            <Text fontSize="lg" fontWeight="bold" mb={2}>
+                A collaborative creation by the GT Big Data team:
+            </Text>
+            <List spacing={2}>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Aryan Shah
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Tony Ngari
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Sarvesh Sathish
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Arshiya Rahman
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Sarayu
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color="green.500" />
+                    Emily Yang
+                </ListItem>
+            
+            </List>
+  </Box>
+            </ModalBody>
+            <ModalFooter>
+                <Button colorScheme="blue" onClick={closeWhoDidThisModal}>
+                Close
+                </Button>
+            </ModalFooter>
+            </ModalContent>
+        </Modal>
+    
+    </>
     )
 }
